86ccab47e1d9908f402a15f1a5ab3e2903b114fe
nic3803 <nicoprandi68@gmail.com>
2022-05-15 18:41:37 -0300

    agrege client/user/product

---
+++ clients/product_client.go
@@ -0,0 +1,18 @@
+package product
+
+import (
+	//	"mvc-go/model"
+
+	"github.com/jinzhu/gorm"
+	//	log "github.com/sirupsen/logrus"
+)
+
+var Db *gorm.DB
+
+func GetProductByKeyPro(key string) /*model.Product*/ {
+
+}
+
+func GetProductByKeyCat(cat string) /*model.Product*/ {
+
+}
---
+++ clients/user_client.go
@@ -0,0 +1,14 @@
+package product
+
+import (
+	//"mvc-go/model"
+
+	"github.com/jinzhu/gorm"
+	//	log "github.com/sirupsen/logrus"
+)
+
+var Db *gorm.DB
+
+func GetUserHistory() string {
+
+}
