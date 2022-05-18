0da4e30d143c727b504177562fd05adf85930e39
nic3803 <nicoprandi68@gmail.com>
2022-05-18 14:32:34 -0300

    se agrego clients/product, igual tengo que terminarlo

--- clients/product/product_client.go
+++ clients/product/product_client.go
@@ -1,13 +1,39 @@
 package product
 
-import "pan/model"
+import (
+	"pan/model"
 
-//	log "github.com/sirupsen/logrus"
+	"github.com/jinzhu/gorm"
+	log "github.com/sirupsen/logrus"
+)
+
+var Db *gorm.DB
 
 func GetProductByName(key string) model.Product {
 
+	var product model.Product
+
+	Db.Where("key = ", key).First(&product)
+	log.Debug("Product: ", product)
+
+	return product
+
 }
 
-func GetProductByCat(cat string) /*model.Product*/ {
+// CORREGIR ESTA FUNCION ESTA MAL
+//REVISAR LIBRERIA GORM PARA APRENDER A BUSCAR POR CAT
+//:)
+//------------------------------------------------------
+func GetProductByCat(cat string) model.Products {
+
+	var products model.Products
+
+	Db.Where("cat = ", cat).Find(&products)
+
+	log.Debug("Products", products)
+
+	return products
 
 }
+
+//-------------------------------------------
