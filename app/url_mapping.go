8d733e8e01cabc729747d90e502f751b26297509
Francisco González <2000908@ucc.edu.ar>
2022-05-31 15:24:48 -0300

    mappeo neworder

--- app/url_mapping.go
+++ app/url_mapping.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	//adressController "mvc-go/controllers/adress"
+	orderController "pan/controllers/order"
 	productController "pan/controllers/product"
 	userController "pan/controllers/user"
 
@@ -16,11 +17,11 @@ func mapUrls() {
 
 	router.POST("/product/product/:KeyPro", productController.GetProductByName) // Busca productos por Palabra clave
 
-	router.POST("/compra/add") //ordenController.NewOrder) //Debe crear la nueva orden verificar cantidades
+	router.POST("/compra/add", orderController.NewOrder) //Debe crear la nueva orden verificar cantidades
 
 	router.PUT("/cart") //CartController.CartMananger) //Sumar Restar sacar y agregar productos en carrito
 
-	router.GET("/User/OrH") //userContrller.GetHistory) //Devolver compras por user
+	router.GET("/User/OrH", orderController.GetHistory) //Devolver compras por user
 
 	router.POST("/product/cat/:KeyCat", productController.GetProductByCat) //product.Controller.GetProductByCat)//Traer Productos por Categoria
 
